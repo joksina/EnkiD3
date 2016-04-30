eb7abf40c6917eacd440751cf4ad56528549f799
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-04-29 17:20:16 -0700

    getting the right data

---
+++ server/server.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var request = require("request");
+var cors = require('cors');
+
+//configure the app to use bodyParser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+app.use(cors());
+app.all('/*', function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+ res.header("Access-Control-Allow-Headers", "Content-type, Accept, X-Access-Token, X-key");
+ if(req.method === "OPTIONS") {
+   res.status(200).end();
+ } else {
+   next();
+ }
+});
+
+//Use GET reguest to get the data from the json file
+app.get('/api/v1/data', function(req, res){
+ request({url:'https://app.upguard.com/api/v1/nodes/6347/scan_details.json', 
+   headers: {
+     Authorization: 'Token token="50d16425888ab0798d5660d70e9aa43b92a54d5dc518cac5bd1f35a7fea3447633ca7606ace40bebc8f52a4639c0cfb49b855af47a9aa5098e97d75b51101bff"',
+     contentType:"json"
+   }
+ },
+   function(err, resp, body) {
+     if (!err && resp.statusCode == 200) {
+       var newBody = body;
+       newBody = JSON.parse(newBody);
+       newBody.data = JSON.parse(newBody.data)        
+     }
+     res.json(newBody);
+ });
+
+})
+
+app.listen(8080, function() {
+ console.log("i am running");
+})
+
+
+module.exports = app;
