0e569f8569331324f46963293448ffdc30fcf8d2
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-05-01 20:46:11 -0700

    creating the server to emit nodes

--- server/server.js
+++ server/server.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
 app.use(cors());
 app.all('/*', function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
- res.header("Access-Control-Allow-Headers", "Content-type, Accept, X-Access-Token, X-key");
- if(req.method === "OPTIONS") {
-   res.status(200).end();
- } else {
-   next();
- }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-type, Accept, X-Access-Token, X-key");
+  if(req.method === "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 
 app.use(express.static(__dirname +'/../client'));
@@ -25,18 +25,18 @@ app.use(express.static(__dirname +'/../client'));
 app.get('/api/v1/data', function(req, res){
  request({url:'https://app.upguard.com/api/v1/nodes/6347/scan_details.json', 
    headers: {
-     Authorization: 'Token token="50d16425888ab0798d5660d70e9aa43b92a54d5dc518cac5bd1f35a7fea3447633ca7606ace40bebc8f52a4639c0cfb49b855af47a9aa5098e97d75b51101bff"',
-     contentType:"json"
+    Authorization: 'Token token="50d16425888ab0798d5660d70e9aa43b92a54d5dc518cac5bd1f35a7fea3447633ca7606ace40bebc8f52a4639c0cfb49b855af47a9aa5098e97d75b51101bff"',
+    contentType:"json"
    }
  },
-   function(err, resp, body) {
-     if (!err && resp.statusCode == 200) {
-       var newBody = body;
-       newBody = JSON.parse(newBody);
-       newBody.data = JSON.parse(newBody.data)        
-     }
-     res.json(newBody);
- });
+  function(err, resp, body) {
+    if (!err && resp.statusCode == 200) {
+      var newBody = body;
+      newBody = JSON.parse(newBody);
+      newBody.data = JSON.parse(newBody.data)        
+    }
+    res.json(newBody);
+});
 
 })
 
