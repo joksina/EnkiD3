ec737d601aa7d0e0c4df04f2db317b415536d38b
Ajoke Salaudeen <jksalaudeen@yahoo.com>
2016-05-04 23:39:28 -0700

    editing the data

--- client/app/data/controllers/dataCtrl.js
+++ client/app/data/controllers/dataCtrl.js
@@ -6,24 +6,14 @@ angular.module('UpGuardApp')
  angular.element(document.querySelector('#panel'))
   .on('hover', function(e) {
     $scope.node = getNode(e.detail, $scope.data);
-    if($scope.data.children === null){
-      $scope.detail = false;
-    }
     $scope.detail = true;
     $scope.$digest();
   })
-  .on('selectNode', function(e) {
-    $scope.$digest();
-  })
-  .on('unSelectNode', function(e) {
-    $scope.$digest();
-  });
 
   socket.on('update', function(data) {
     $scope.data = angular.copy(data);
   });
 
-//searching the tree
   var getNode = function(name, data) {
     if (data.name === name) {
       return data;
@@ -34,6 +24,10 @@ angular.module('UpGuardApp')
     for (var i = 0; i < data.children.length; i++) {
       var match = getNode(name, data.children[i]);
       if (match) {
+        if(typeof match.value === false){
+          JSON.parse(match.value);
+        }
+        // console.log(match.value);
         return match;
       }
     }
